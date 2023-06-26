24e51a7cb72c5b55b3098e1d004b9d3205a8ac1a
Ivan Ratushinskiy <iratushinakiy@appnow.com>
2023-06-26 04:56:51 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Ratushinsky_36_1_Dates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DateNow = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Formats = new System.Windows.Forms.Button();
+            this.CkeckPerf = new System.Windows.Forms.Button();
+            this.CheckPerf2 = new System.Windows.Forms.Button();
+            this.Plus7Days = new System.Windows.Forms.Button();
+            this.DateDiff = new System.Windows.Forms.Button();
+            this.DateDiff2 = new System.Windows.Forms.Button();
+            this.CheckYear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DateNow
+            // 
+            this.DateNow.Location = new System.Drawing.Point(67, 100);
+            this.DateNow.Name = "DateNow";
+            this.DateNow.Size = new System.Drawing.Size(229, 32);
+            this.DateNow.TabIndex = 0;
+            this.DateNow.Text = "Текущая дата";
+            this.DateNow.UseVisualStyleBackColor = true;
+            this.DateNow.Click += new System.EventHandler(this.DateNow_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(375, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(413, 407);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Formats
+            // 
+            this.Formats.Location = new System.Drawing.Point(514, 0);
+            this.Formats.Name = "Formats";
+            this.Formats.Size = new System.Drawing.Size(174, 32);
+            this.Formats.TabIndex = 2;
+            this.Formats.Text = "Форматы даты и времени";
+            this.Formats.UseVisualStyleBackColor = true;
+            this.Formats.Click += new System.EventHandler(this.Formats_Click);
+            // 
+            // CkeckPerf
+            // 
+            this.CkeckPerf.Location = new System.Drawing.Point(67, 138);
+            this.CkeckPerf.Name = "CkeckPerf";
+            this.CkeckPerf.Size = new System.Drawing.Size(229, 32);
+            this.CkeckPerf.TabIndex = 3;
+            this.CkeckPerf.Text = " Измерение производительности 1";
+            this.CkeckPerf.UseVisualStyleBackColor = true;
+            this.CkeckPerf.Click += new System.EventHandler(this.CkeckPerf_Click);
+            // 
+            // CheckPerf2
+            // 
+            this.CheckPerf2.Location = new System.Drawing.Point(67, 176);
+            this.CheckPerf2.Name = "CheckPerf2";
+            this.CheckPerf2.Size = new System.Drawing.Size(229, 32);
+            this.CheckPerf2.TabIndex = 4;
+            this.CheckPerf2.Text = " Измерение производительности 2";
+            this.CheckPerf2.UseVisualStyleBackColor = true;
+            this.CheckPerf2.Click += new System.EventHandler(this.CheckPerf2_Click);
+            // 
+            // Plus7Days
+            // 
+            this.Plus7Days.Location = new System.Drawing.Point(67, 214);
+            this.Plus7Days.Name = "Plus7Days";
+            this.Plus7Days.Size = new System.Drawing.Size(229, 32);
+            this.Plus7Days.TabIndex = 5;
+            this.Plus7Days.Text = " Добавление 7 дней к текущей дате ";
+            this.Plus7Days.UseVisualStyleBackColor = true;
+            this.Plus7Days.Click += new System.EventHandler(this.Plus7Days_Click);
+            // 
+            // DateDiff
+            // 
+            this.DateDiff.Location = new System.Drawing.Point(67, 252);
+            this.DateDiff.Name = "DateDiff";
+            this.DateDiff.Size = new System.Drawing.Size(229, 32);
+            this.DateDiff.TabIndex = 6;
+            this.DateDiff.Text = "DateDiff1";
+            this.DateDiff.UseVisualStyleBackColor = true;
+            this.DateDiff.Click += new System.EventHandler(this.DateDiff_Click);
+            // 
+            // DateDiff2
+            // 
+            this.DateDiff2.Location = new System.Drawing.Point(67, 290);
+            this.DateDiff2.Name = "DateDiff2";
+            this.DateDiff2.Size = new System.Drawing.Size(229, 32);
+            this.DateDiff2.TabIndex = 7;
+            this.DateDiff2.Text = "DateDiff2";
+            this.DateDiff2.UseVisualStyleBackColor = true;
+            this.DateDiff2.Click += new System.EventHandler(this.DateDiff2_Click);
+            // 
+            // CheckYear
+            // 
+            this.CheckYear.Location = new System.Drawing.Point(67, 328);
+            this.CheckYear.Name = "CheckYear";
+            this.CheckYear.Size = new System.Drawing.Size(229, 32);
+            this.CheckYear.TabIndex = 8;
+            this.CheckYear.Text = "Проверка на високосный год";
+            this.CheckYear.UseVisualStyleBackColor = true;
+            this.CheckYear.Click += new System.EventHandler(this.CheckYear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CheckYear);
+            this.Controls.Add(this.DateDiff2);
+            this.Controls.Add(this.DateDiff);
+            this.Controls.Add(this.Plus7Days);
+            this.Controls.Add(this.CheckPerf2);
+            this.Controls.Add(this.CkeckPerf);
+            this.Controls.Add(this.Formats);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.DateNow);
+            this.Name = "Form1";
+            this.Text = "STOLYAROV NIKITA 36/1 DATES";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DateNow;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Formats;
+        private System.Windows.Forms.Button CkeckPerf;
+        private System.Windows.Forms.Button CheckPerf2;
+        private System.Windows.Forms.Button Plus7Days;
+        private System.Windows.Forms.Button DateDiff;
+        private System.Windows.Forms.Button DateDiff2;
+        private System.Windows.Forms.Button CheckYear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ratushinsky_36_1_Dates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DateNow_Click(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Now;
+            MessageBox.Show(today.Date.ToLongDateString());
+        }
+
+        private void Formats_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            listBox1.Items.Clear();
+            listBox1.Items.Add("d: " + dt.ToString("d"));
+            listBox1.Items.Add("D: " + dt.ToString("D"));
+            listBox1.Items.Add("f: " + dt.ToString("f"));
+            listBox1.Items.Add("F: " + dt.ToString("F"));
+            listBox1.Items.Add("g: " + dt.ToString("g"));
+            listBox1.Items.Add("G: " + dt.ToString("G"));
+            listBox1.Items.Add("m: " + dt.ToString("m"));
+            listBox1.Items.Add("r: " + dt.ToString("r"));
+            listBox1.Items.Add("s: " + dt.ToString("s"));
+            listBox1.Items.Add("u: " + dt.ToString("u"));
+            listBox1.Items.Add("U: " + dt.ToString("U"));
+            listBox1.Items.Add("y: " + dt.ToString("y"));
+            listBox1.Items.Add("MMMM dd: " + dt.ToString("MMMM dd"));
+            listBox1.Items.Add("MM/dd/yyyy: " + dt.ToString("MM/dd/yyyy"));
+            listBox1.Items.Add("MM/dd/yyyy HH:mm: " + dt.ToString("MM/dd/yyyy HH:mm"));
+            listBox1.Items.Add("MM/dd/yyyy hh:mm tt: " + dt.ToString("MM/dd/yyyy hh:mm tt"));
+            listBox1.Items.Add("MM/dd/yyyy H:mm: " + dt.ToString("MM/dd/yyyy H:mm"));
+            listBox1.Items.Add("MM/dd/yyyy h:mm tt: " + dt.ToString("MM/dd/yyyy h:mm tt"));
+            listBox1.Items.Add("MM/dd/yyyy HH:mm:ss: " + dt.ToString("MM/dd/yyyy HH:mm:ss"));
+            listBox1.Items.Add("dddd, dd MMMM yyyy: " + dt.ToString("dddd, dd MMMM yyyy"));
+            listBox1.Items.Add("dddd, dd MMMM yyyy HH:mm:ss: " + dt.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+            listBox1.Items.Add("ddd, dd MMM yyyy HH':'mm':'ss 'GMT': " + dt.ToString("ddd, dd MMM yyyy HH':'mm':'ss 'GMT'"));
+            listBox1.Items.Add("yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffffffK: " + dt.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffffffK"));
+            listBox1.Items.Add("yyyy'-'MM'-'dd'T'HH':'mm':'ss: " + dt.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss"));
+            listBox1.Items.Add("yyyy MMMM: " + dt.ToString("yyyy MMMM"));
+            listBox1.Items.Add("H:mm: " + dt.ToString("H:mm"));
+            listBox1.Items.Add("HH:mm: " + dt.ToString("HH:mm"));
+            listBox1.Items.Add("HH:mm:ss: " + dt.ToString("HH:mm:ss"));
+            listBox1.Items.Add("h:mm tt: " + dt.ToString("h:mm tt"));
+            listBox1.Items.Add("hh:mm tt: " + dt.ToString("hh:mm tt"));
+        }
+
+        private void CkeckPerf_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.UtcNow;
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < 10; i++)
+            {
+                sw.Start();
+                for (int j = 0; j < 100000; j++)
+                {
+                    if (DateTime.UtcNow == dt)
+                    {
+                        /* do action */
+                    }
+                }
+                MessageBox.Show(sw.ElapsedTicks.ToString());
+            }
+        }
+
+        private void CheckPerf2_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < 10; i++)
+            {
+                sw.Start();
+                for (int j = 0; j < 100000; j++)
+                {
+                    if (DateTime.Now == dt)
+                    {
+                        /* do action */
+                    }
+                }
+                MessageBox.Show(sw.ElapsedTicks.ToString());
+            }
+        }
+
+        private void Plus7Days_Click(object sender, EventArgs e)
+        {
+            DateTime curdate = DateTime.Now;
+            DateTime mydate = curdate.AddDays(7);
+            MessageBox.Show(mydate.ToShortDateString());
+        }
+
+        private void DateDiff_Click(object sender, EventArgs e)
+        {
+            DateTime date1 = new DateTime(2004, 9, 24); // Первая дата 
+            DateTime date2 = DateTime.Now; // Вторая дата 
+                                           // Вычисляем разницу в днях при помощи перечисления DateInterval.Day 
+            long diff = DateAndTime.DateDiff(DateInterval.Day, date1, date2,
+            FirstDayOfWeek.Monday, FirstWeekOfYear.System);
+            // Выводим результат в заголовке формы 
+            this.Text = diff.ToString() + " дней";
+        }
+
+        private void DateDiff2_Click(object sender, EventArgs e)
+        {
+            string howdays = DaysDiff(DateTime.Today, new DateTime(2004, 09, 24)).ToString();
+            MessageBox.Show("Со дня моего дня рождения прошло: " + howdays + " дней");
+        }
+        public static int DaysDiff(DateTime date1, DateTime date2)
+        {
+            return date1.Subtract(date2.Date).Days;
+        }
+
+        private void CheckYear_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(EasterDate(2006).ToLongDateString());
+        }
+        public static DateTime EasterDate(int year)
+        {
+            int Y = year;
+            int a = Y % 19;
+            int b = Y / 100;
+            int c = Y % 100;
+            int d = b / 4;
+            int e = b % 4;
+            int f = (b + 8) / 25;
+            int g = (b - f + 1) / 3;
+            int h = (19 * a + b - d - g + 15) % 30;
+            int i = c / 4;
+            int k = c % 4;
+            int L = (32 + 2 * e + 2 * i - h - k) % 7;
+            int m = (a + 11 * h + 22 * L) / 451;
+            int month = (h + L - 7 * m + 114) / 31;
+            int day = ((h + L - 7 * m + 114) % 31) + 1;
+            DateTime dt = new DateTime(year, month, day);
+            return dt;
+        }
+    }
+}
